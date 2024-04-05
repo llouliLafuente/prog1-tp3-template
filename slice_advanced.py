8358e6034d7038f853a6799ace5d3996ca0c74f2
llouliLafuente <141773015+llouliLafuente@users.noreply.github.com>
2024-04-05 12:18:09 -0300

    Update slice_advanced.py

--- slice_advanced.py
+++ slice_advanced.py
@@ -3,3 +3,8 @@ def slice_advanced():
 
 # Para verificar este ejercicio ejecutar el comando
 # `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
+    txt = "Awesome"
+    print(txt[0:3].lower())
+    print(txt[2:5])
+    print(txt[0:4].lower() + txt[4:7].lower())
+slice_advanced()
