56eac54df7e1ff04d5175cc2751f9620b32f0848
llouliLafuente <141773015+llouliLafuente@users.noreply.github.com>
2024-04-05 12:15:30 -0300

    Update in_string.py

--- in_string.py
+++ in_string.py
@@ -1,6 +1,10 @@
 def check_vowels():
-    # Código a implementar utilizando input.
+    txt = input("Ingresa algo: ")
 
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_in_string_test.py` o `python tp3_in_string_test.py`
+    a = ("a" in txt or "A" in txt)
+    b = ("e" in txt or "E" in txt)
+    c = ("i" in txt or "I" in txt)
+    d = ("o" in txt or "O" in txt)
+    e = ("u" in txt or "U" in txt)
+    print(f'\n Contiene a: {a} \n Contiene e: {b} \n Contiene i: {c} \n Contiene o: {d} \n Contiene U: {e}')
+check_vowels()
